18a5ea04c6d61491773b14d7237b1082b3a829b4
budi.ss <budi.ss@009ACE1013.nu-ace.ad-ins.com>
2014-03-10 15:47:08 +0700

    Employee Insert, masih ada yang hardcode

--- ProjectTaps/src/adins/ace/taps/manager/EmployeeManager.java
+++ ProjectTaps/src/adins/ace/taps/manager/EmployeeManager.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
+import adins.ace.taps.bean.employee.NewEmployeeBean;
 import adins.ace.taps.bean.organization.OrganizationBean;
 import adins.ace.taps.ibatis.IbatisHelper;
 
@@ -17,7 +18,24 @@ public class EmployeeManager {
 	public EmployeeManager() {
 		this.ibatisSqlMap = IbatisHelper.getSqlMapInstance();
 	}
-
+	public boolean insertNewEmployee(NewEmployeeBean newEmployee){
+		boolean flag=false;
+		try {
+			ibatisSqlMap.startTransaction();
+			ibatisSqlMap.queryForObject("employee.addEmployee", newEmployee);
+			ibatisSqlMap.commitTransaction();
+			flag=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ibatisSqlMap.endTransaction();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return flag;
+	}
 	
 	public List getAllEmployees(){
 		List empList = null;
