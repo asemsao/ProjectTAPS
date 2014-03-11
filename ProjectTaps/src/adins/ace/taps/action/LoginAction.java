9e30fe2f3a7dcb9964b6bd09ee98bab4c18b7cf9
budi.ss <budi.ss@009ACE1013.nu-ace.ad-ins.com>
2014-03-11 18:01:33 +0700

    On progress create session. Tambahan buat @wiryajaya17 @asemsao jadinya menu mau dinamis atau statis ni?yg dimaksud kmrn tambah menu gimana?

--- ProjectTaps/src/adins/ace/taps/action/LoginAction.java
+++ ProjectTaps/src/adins/ace/taps/action/LoginAction.java
@@ -3,6 +3,10 @@
 
 package adins.ace.taps.action;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.*;
 
 import org.apache.struts.action.Action;
@@ -11,6 +15,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import adins.ace.taps.form.login.LoginForm;
+import adins.ace.taps.manager.LoginManager;
 import adins.ace.taps.module.LoginModule;
 
 public class LoginAction extends Action {
@@ -20,16 +25,21 @@ public class LoginAction extends Action {
 			throws Exception {
 
 		LoginForm tForm = (LoginForm) form;
+		LoginManager lMan = new LoginManager();
 		LoginModule loginAuth = new LoginModule();
-		//HttpSession session = request.getSession(true);
-
+		HttpSession session = request.getSession(true);
+		Map params = new HashMap();
+		
 		if ("login".equals(tForm.getTask())) {
 			boolean pass = false;
 			if (!"".equals(tForm.getUsername())
 					&& !"".equals(tForm.getPassword())) {
 
-				String username = tForm.getUsername();
-				String password = tForm.getPassword();
+				/*String username = tForm.getUsername();
+				String password = tForm.getPassword();*/
+				//Testing ntar dihapus
+				String username = "budi.ss";
+				String password = "Terev9977";
 				String domainName = "nu-ace.ad-ins.com";
 
 				pass = loginAuth.getAuthenticationUser(username, password, domainName);
@@ -41,11 +51,37 @@ public class LoginAction extends Action {
 					 * SET SESSION
 					 * session.setAttribute("username", username);
 					 */
+					///TESTING HAPUS NANTI
+					username = "domain205";
+					/////
+					session.setAttribute("supervisor", "false");
+					session.setAttribute("employee", "false");
+					session.setAttribute("head bu", "false");
+					session.setAttribute("head dept", "false");
+					session.setAttribute("admnistrator", "false");
+					session.setAttribute("bom", "false");
 					
+					params.put("userDomain", username);
+					
+					List<String> roleList = lMan.roleList(params);
+					for (int i = 0; i < roleList.size(); i++) {
+						session.setAttribute(roleList.get(i), "true");
+					}
+					for (int i = 0; i < roleList.size(); i++) {
+						System.out.print(roleList.get(i));
+						System.out.println(session.getAttribute(roleList.get(i)));
+					}
+					/*
+					session.setAttribute("username", username);
+					session.setAttribute("spv", "true");
+					session.setAttribute("emp", "true");
+					session.setAttribute("hbu", "true");
+					session.setAttribute("hde", "true");
+					session.setAttribute("adm", "true");
+					session.setAttribute("bom", "true");*/
 					return mapping.findForward("Dashboard");
 				} else {
-					return mapping.findForward("Dashboard");
-					//return mapping.findForward("Welcome");
+					return mapping.findForward("Welcome");
 				}
 			} else {
 				return mapping.findForward("Welcome");
