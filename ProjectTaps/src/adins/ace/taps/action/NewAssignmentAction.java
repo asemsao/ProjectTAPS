d2d76b72778197ee509d306a350e82df1825f17a
meyliana.tanjung <meyliana.tanjung@014ACE1013.nu-ace.ad-ins.com>
2014-03-18 14:48:21 +0700

    ganti new assignment dan new self ke session user domain

--- ProjectTaps/src/adins/ace/taps/action/NewAssignmentAction.java
+++ ProjectTaps/src/adins/ace/taps/action/NewAssignmentAction.java
@@ -29,7 +29,10 @@ public class NewAssignmentAction extends Action {
 		DateFormat dateFormat = new SimpleDateFormat("yyMM");
 		Date date = new Date();
 		boolean success = false;
-
+		//coba pake domain3
+		session.setAttribute("username", "domain3");
+		//nanti dihapus
+		
 		if (aForm.getNewTask() == null) {
 			if (session.getAttribute("taskCode") != null) {
 				aForm.setAssignmentBean(aMan
@@ -45,21 +48,23 @@ public class NewAssignmentAction extends Action {
 			} else {
 				aForm.getAssignmentBean().setAssignmentType(
 						aForm.getAssignmentType());
-				aForm.getAssignmentBean().setReportTo("domain100");
+				aForm.getAssignmentBean().setReportTo((String) session.getAttribute("username"));
 				String paramCode = "";
 				if ("BU".equals(aForm.getAssignmentType())) {
 					aForm.getAssignmentBean().setOrganizationCode(
-							aMan.searchOrganizationCode("domain3"));
+							aMan.searchOrganizationCode((String) session.getAttribute("username")));
 					aForm.getAssignmentBean().setProjectCode(null);
 					paramCode = aForm.getAssignmentBean().getOrganizationCode()
 							+ dateFormat.format(date);
+					paramCode = paramCode + aMan.getMaxTaskCodeOrganization(paramCode);
 				} else if ("PROJECT".equals(aForm.getAssignmentType())) {
 					paramCode = aForm.getAssignmentBean().getProjectCode()
 							+ dateFormat.format(date);
+					paramCode = paramCode + aMan.getMaxTaskCodeProject(paramCode);
 				}
-				paramCode = paramCode + aMan.getMaxTaskCode(paramCode);
+				
 				aForm.getAssignmentBean().setTaskCode(paramCode);
-				aForm.getAssignmentBean().setCreatedBy("domain100");
+				aForm.getAssignmentBean().setCreatedBy((String) session.getAttribute("username"));
 
 				if ("save".equals(aForm.getNewTask())) {
 					aForm.getAssignmentBean().setCurrentStatus("DRAFT");
@@ -72,7 +77,7 @@ public class NewAssignmentAction extends Action {
 				if (session.getAttribute("taskCode") != null) {
 					aForm.getAssignmentBean().setTaskCode(
 							(String) session.getAttribute("taskCode"));
-					aForm.getAssignmentBean().setUpdatedBy("domain100");
+					aForm.getAssignmentBean().setUpdatedBy((String) session.getAttribute("username"));
 					success = aMan.editAssignment(aForm.getAssignmentBean());
 				} else {
 					success = aMan.addAssignment(aForm.getAssignmentBean());
