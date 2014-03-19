d13336503e6f30496320a687a8a25d475bed17f3
ketut.gwj <ketut.gwj@005ACE1013.nu-ace.ad-ins.com>
2014-03-19 07:57:57 +0700

    edit report, employeeRole >> manage Role

---
+++ ProjectTaps/src/adins/ace/taps/bean/manageRole/MenuBean.java
@@ -0,0 +1,40 @@
+package adins.ace.taps.bean.manageRole;
+
+import java.io.Serializable;
+
+public class MenuBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String menuId;
+	private String menuName;
+	private String task;
+	private String description;
+	
+	public String getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
