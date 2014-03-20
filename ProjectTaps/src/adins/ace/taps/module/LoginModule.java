0b8a95bccacc16e6f3f9b112c728dd3571e191ed
Kamshwanee Yukoshima <daversetyawan@gmail.com>
2014-03-20 17:10:24 +0700

    recovery mode ya

--- ProjectTaps/src/adins/ace/taps/module/LoginModule.java
+++ ProjectTaps/src/adins/ace/taps/module/LoginModule.java
@@ -1,11 +1,16 @@
 package adins.ace.taps.module;
 
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
+
+import adins.ace.taps.configuration.App;
+import adins.ace.taps.manager.LoginManager;
 
 public class LoginModule {
 	private String username;
@@ -17,25 +22,41 @@ public class LoginModule {
 
 	public boolean getAuthenticationUser(String username, String password,
 			String domainName) {
-		boolean pass = false;
+		boolean logged = false;
 		this.username = username;
 		this.password = password;
 		this.domainName = domainName;
+		if (isRecoveryMode()) {
+			LoginManager lMan = new LoginManager();
+			Map params = new HashMap();
+			params.put("username", this.username);
+			params.put("password", this.password);
+			logged = lMan.tryLogin(params);
+		} else {
+			GetUserDomainModule domainAuth = new GetUserDomainModule();
+			Hashtable<String, String> env = new Hashtable<String, String>();
 
-		GetUserDomainModule domainAuth = new GetUserDomainModule();
-		Hashtable<String, String> env = new Hashtable<String, String>();
-
-		env = domainAuth.getDomainAuthentication(this.username, this.password,
-				this.domainName);
-		DirContext ctx = null;
-		try {
-			ctx = new InitialDirContext(env);
-			if (!ctx.equals(null)) {
-				pass = true;
+			env = domainAuth.getDomainAuthentication(this.username, this.password,
+					this.domainName);
+			DirContext ctx = null;
+			try {
+				ctx = new InitialDirContext(env);
+				if (!ctx.equals(null)) {
+					logged = true;
+				}
+			} catch (NamingException ex) {
+				System.out.println(ex.toString());
 			}
-		} catch (NamingException ex) {
-			System.out.println(ex.toString());
 		}
-		return pass;
+		return logged;
 	}
+
+	private boolean isRecoveryMode() {
+		if ("true".equals(App.getConfiguration("recovery_mode"))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
