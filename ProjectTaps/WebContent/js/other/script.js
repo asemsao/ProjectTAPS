3a58175d5cb42504f6884bda967c358ba8e0b12d
Kamshwanee Yukoshima <daversetyawan@gmail.com>
2014-03-21 18:25:26 +0700

    datepicker change on index assignment

--- ProjectTaps/WebContent/js/other/script.js
+++ ProjectTaps/WebContent/js/other/script.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
 	$("#__input_file_wrapper__").attr('placeholder', 'Browse File');
-	$("#datepicker-begin, #datepicker-end, #datepicker").datepicker({
-		format : "dd/mm/yyyy",
-		effect : "none",
-		position : "bottom"
-	});
-
 });
