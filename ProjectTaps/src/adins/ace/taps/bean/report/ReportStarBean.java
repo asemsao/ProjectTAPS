a8af600bbe0bbf34cb3e11785cf18c916cd0db80
vinsen.surya <vinsen.surya@015ACE1013.nu-ace.ad-ins.com>
2014-04-03 19:44:19 +0700

    report

---
+++ ProjectTaps/src/adins/ace/taps/bean/report/ReportStarBean.java
@@ -0,0 +1,94 @@
+package adins.ace.taps.bean.report;
+
+import java.io.Serializable;
+
+public class ReportStarBean implements Serializable
+{
+	//detail star red and green
+	private Integer star01;
+	private Integer star02;
+	private Integer star03;
+	private Integer star04;
+	private Integer star05;
+	private Integer star06;
+	private Integer star07;
+	private Integer star08;
+	private Integer star09;
+	private Integer star10;
+	private Integer star11;
+	private Integer star12;
+	public Integer getStar01() {
+		return star01;
+	}
+	public void setStar01(Integer star01) {
+		this.star01 = star01;
+	}
+	public Integer getStar02() {
+		return star02;
+	}
+	public void setStar02(Integer star02) {
+		this.star02 = star02;
+	}
+	public Integer getStar03() {
+		return star03;
+	}
+	public void setStar03(Integer star03) {
+		this.star03 = star03;
+	}
+	public Integer getStar04() {
+		return star04;
+	}
+	public void setStar04(Integer star04) {
+		this.star04 = star04;
+	}
+	public Integer getStar05() {
+		return star05;
+	}
+	public void setStar05(Integer star05) {
+		this.star05 = star05;
+	}
+	public Integer getStar06() {
+		return star06;
+	}
+	public void setStar06(Integer star06) {
+		this.star06 = star06;
+	}
+	public Integer getStar07() {
+		return star07;
+	}
+	public void setStar07(Integer star07) {
+		this.star07 = star07;
+	}
+	public Integer getStar08() {
+		return star08;
+	}
+	public void setStar08(Integer star08) {
+		this.star08 = star08;
+	}
+	public Integer getStar09() {
+		return star09;
+	}
+	public void setStar09(Integer star09) {
+		this.star09 = star09;
+	}
+	public Integer getStar10() {
+		return star10;
+	}
+	public void setStar10(Integer star10) {
+		this.star10 = star10;
+	}
+	public Integer getStar11() {
+		return star11;
+	}
+	public void setStar11(Integer star11) {
+		this.star11 = star11;
+	}
+	public Integer getStar12() {
+		return star12;
+	}
+	public void setStar12(Integer star12) {
+		this.star12 = star12;
+	}
+	
+	
+}
